8d1739693fa9cd6562e770c4a966983da1dbfe79
anettmava <anettmava@gmail.com>
2026-03-13 18:57:10 -0600

    Fixed port

--- demo/backend/src/main/java/com/example2/demo/DemoApplication.java
+++ demo/backend/src/main/java/com/example2/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.example2.demo", "com.formapi"})
+
 public class DemoApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- demo/backend/src/main/java/com/example2/demo/controllers/helloWorld.java
+++ demo/backend/src/main/java/com/example2/demo/controllers/helloWorld.java
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+
 public class helloWorld {
+
     @Autowired
     private HelloWorldServices service;
 
     @GetMapping("/")
+  
     public String getMethodName(@RequestParam String name) {
         return service.generateMessage(name);
     }
